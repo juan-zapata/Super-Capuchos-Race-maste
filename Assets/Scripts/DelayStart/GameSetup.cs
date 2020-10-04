55cf16ebf548968e49fe22d36bc2a851e1ab5bff
Juan Pablo Quiroz Piedrahita <jpablo.quiroz@udea.edu.co>
2020-10-04 13:16:17 -0500

    fixed region and gamesetup

--- Assets/Scripts/DelayStart/GameSetup.cs
+++ Assets/Scripts/DelayStart/GameSetup.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class GameSetup : MonoBehaviour
 {
+    GameObject player1;
+    GameObject player2;
     void Start(){
         CreatePlayer();
     }
 
     private void CreatePlayer(){
-        Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Creating Player");
+            player1 = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "P1Bullet"), new Vector3(41,16,0), Quaternion.identity, 0);
+            
+        }
+        else {
+            Debug.Log("Creating Player");
+            player2 = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "P2Bullet"), new Vector3(41,-13,0), Quaternion.identity, 0);
+            
+        }
+        
     }
 }
